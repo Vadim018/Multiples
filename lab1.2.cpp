15568c985dab93d2c23dba2f31d4fed655a5ddb2
Vadim Voitsehovskiy <vadim.rolex.2005@gmail.com>
2023-12-27 16:13:32 +0200

    Add project files.

---
+++ lab1.2.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+
+    setlocale(LC_ALL, "Ukr");
+    int n, m;
+
+    cout << "Введ*ть значення n: ";
+    cin >> n;
+    cout << "\n";
+
+    cout << "Введ*ть значення m (б*льше за n): ";
+    cin >> m;
+    cout << "\n";
+
+    if (n >= m) {
+        cout << "m має бути б*льше за n!" << endl;
+        return 1;
+    }
+
+    int sum = 0;
+    
+    cout << "Кратн* 4 числа в*д " << n << " до " << m << ":" << endl;
+
+    for (int i = n; i <= m; ++i) {
+        if (i % 4 == 0) {
+            cout << i << " ";
+            sum += i;
+        }
+    }
+    cout << "\n";
+    cout << "\nСума кратних 4 чисел: " << sum << endl;
+    return 0;
+}
